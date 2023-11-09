d3e0d8dd2374ff963e625ec7e6c46ac5472d71da
yel106 <yel_@hanmail.net>
2023-11-09 11:12:36 +0900

    package.json에 "proxy": "http://localhost:8080" 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { HomeLayout } from "./layout/HomeLayout";
+import { BoardList } from "./page/BoardList";
+import { BoardWrite } from "./page/BoardWrite";
+
+const routes = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<HomeLayout />}>
+      <Route index element={<BoardList />} />
+      <Route path="write" element={<BoardWrite />} />
+    </Route>,
+  ),
+);
 
 function App(props) {
-  return (
-    <div></div>
-  );
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
