d32c27fd42af4d6273cd35fa9542e1da3032a4b7
yel106 <yel_@hanmail.net>
2023-11-10 12:35:43 +0900

    암호 확인 로직 추가

--- src/page/member/MemberSignup.js
+++ src/page/member/MemberSignup.js
@@ -1,4 +1,11 @@
-import { Box, Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+} from "@chakra-ui/react";
 import { useState } from "react";
 import axios from "axios";
 
@@ -7,6 +14,16 @@ export function MemberSignup() {
   const [password, setPassword] = useState("");
   const [passwordCheck, setPasswordCheck] = useState("");
   const [email, setEmail] = useState("");
+
+  let submitAvailable = true;
+
+  if (password != passwordCheck) {
+    submitAvailable = false;
+  }
+
+  if (password.length === 0) {
+    submitAvailable = false;
+  }
 
   function handleSubmit() {
     axios
@@ -27,7 +44,7 @@ export function MemberSignup() {
         <FormLabel>id</FormLabel>
         <Input value={id} onChange={(e) => setId(e.target.value)} />
       </FormControl>
-      <FormControl>
+      <FormControl isInvalid={password.length === 0}>
         <FormLabel>password</FormLabel>
         <Input
           type="password"
@@ -35,13 +52,15 @@ export function MemberSignup() {
           onChange={(e) => setPassword(e.target.value)}
         />
       </FormControl>
-      <FormControl>
+      <FormControl isInvalid={password != passwordCheck}>
         <FormLabel>password 확인</FormLabel>
         <Input
           type="password"
           value={passwordCheck}
           onChange={(e) => setPasswordCheck(e.target.value)}
         />
+        <FormErrorMessage>암호를 입력해주세요</FormErrorMessage>
+        <FormErrorMessage>암호가 다릅니다.</FormErrorMessage>
       </FormControl>
       <FormControl>
         <FormLabel>email</FormLabel>
@@ -51,7 +70,11 @@ export function MemberSignup() {
           onChange={(e) => setEmail(e.target.value)}
         />
       </FormControl>
-      <Button onClick={handleSubmit} colorScheme="blue">
+      <Button
+        isDisabled={!submitAvailable}
+        onClick={handleSubmit}
+        colorScheme="blue"
+      >
         가입
       </Button>
     </Box>
