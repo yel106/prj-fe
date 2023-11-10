806bf5a88589e35470afa37e163198fece11f1df
yel106 <yel_@hanmail.net>
2023-11-10 15:39:35 +0900

    가입 후 기능 추가(toast, navigate)

--- src/page/member/MemberSignup.js
+++ src/page/member/MemberSignup.js
@@ -10,6 +10,7 @@ import {
 } from "@chakra-ui/react";
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export function MemberSignup() {
   const [id, setId] = useState("");
@@ -21,6 +22,7 @@ export function MemberSignup() {
   const [emailAvailable, setEmailAvailable] = useState(false);
 
   const toast = useToast();
+  const navigate = useNavigate();
 
   let submitAvailable = true;
 
@@ -47,9 +49,28 @@ export function MemberSignup() {
         password,
         email,
       })
-      .then(() => console.log("good"))
-      .catch(() => console.log("bad"))
-      .finally(() => console.log("done"));
+      .then(() => {
+        //toast
+        // navigate)
+        toast({
+          description: "회원 가입이 완료되었습니다.",
+          status: "success",
+        });
+      })
+      .catch((error) => {
+        if (error.response.status == 400) {
+          toast({
+            description: "입력 값을 확인해주세요",
+            status: "error",
+          });
+          navigate("/");
+        } else {
+          toast({
+            description: "가입 중에 오류가 발생하였습니다.",
+            status: "error",
+          });
+        }
+      });
   }
 
   function handleIdCheck() {
