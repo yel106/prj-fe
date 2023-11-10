20762cf0568aadeb6bf13b2e9b214c1e76c1953a
yel106 <yel_@hanmail.net>
2023-11-10 09:42:23 +0900

    제목과 콘텐트 변경 가능 handle... 함수 완성

--- src/page/BoardEdit.js
+++ src/page/BoardEdit.js
@@ -13,7 +13,7 @@ import axios from "axios";
 
 export function BoardEdit() {
   const [board, updateBoard] = useImmer(null);
-
+  const [edit, updateEdit] = useImmer(null);
   // /edit/:id
   const { id } = useParams();
 
@@ -27,17 +27,33 @@ export function BoardEdit() {
     return <Spinner />;
   }
 
+  useEffect(() => {
+    axios.get("api/edit");
+  }, []);
+
+  function handleTitleChange(e) {
+    updateBoard((draft) => {
+      draft.title = e.target.value;
+    });
+  }
+
+  function handleContentChange(e) {
+    updateBoard((draft) => {
+      draft.content = e.target.value;
+    });
+  }
+
   return (
     <Box>
       <h1>{id}번 글 수정</h1>
 
       <FormControl>
         <FormLabel>제목</FormLabel>
-        <Input value={board.title} />
+        <Input value={board.title} onChange={handleTitleChange} />
       </FormControl>
       <FormControl>
         <FormLabel>본문</FormLabel>
-        <Textarea value={board.content} />
+        <Textarea value={board.content} onChange={handleContentChange} />
       </FormControl>
       <FormControl>
         <FormLabel>작성자</FormLabel>
