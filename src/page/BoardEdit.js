a7f508a4f7f592952d9ff64ed20829de21f342b7
yel106 <yel_@hanmail.net>
2023-11-10 11:13:49 +0900

    연습.

--- src/page/BoardEdit.js
+++ src/page/BoardEdit.js
@@ -4,8 +4,16 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Spinner,
   Textarea,
+  useDisclosure,
 } from "@chakra-ui/react";
 import React, { useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
@@ -20,6 +28,8 @@ export function BoardEdit() {
 
   const navigate = useNavigate();
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   useEffect(() => {
     axios
       .get("/api/board/id/" + id)
@@ -30,9 +40,19 @@ export function BoardEdit() {
     return <Spinner />;
   }
 
-  function handleStore() {
-    axios.get("/api/board/store").then(response);
+  function handleSubmit() {
+    axios
+      .put("/api/board/edit", board)
+      .then((response) => { toast( {
+        description: "수정 되었습니다.",
+        status: "success",
+      })
+      })
+      .catch(() => console.log(""))
+      .finally(() => console.log(""));
   }
+
+  function handleEdite() {}
 
   return (
     <Box>
@@ -70,11 +90,27 @@ export function BoardEdit() {
           }
         />
       </FormControl>
-      <Button colorScheme="blue" onClick={handleStore}>
+      <Button colorScheme="blue" onClick={handleSubmit}>
         저장
       </Button>
       {/* navigate(-1) : 이전 경로로 이동 */}
       <Button onClick={() => navigate(-1)}>취소</Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalContent>
+            <ModalHeader>수정 확인</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>수정 하시겠습니까?</ModalBody>
+
+            <ModalFooter>
+              <Button onClick={handleSubmit}>수정하기</Button>
+              <Button onClick={onClose}>닫기</Button>
+            </ModalFooter>
+          </ModalContent>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
