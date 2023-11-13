f1ef3e2d72fef464fc5e89f027e7210021d7335b
yel106 <yel_@hanmail.net>
2023-11-13 09:39:28 +0900

    회원정보 보기 기능 추가 중- 회원 목록 누르면 수정, 삭제 버튼 보이게 하기

--- src/page/member/MemberView.js
+++ src/page/member/MemberView.js
@@ -1,8 +1,42 @@
 import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Spinner,
+  Button,
+} from "@chakra-ui/react";
+import axios from "axios";
 
 export function MemberView() {
   // /member?id=userid 로 넘어오는데 이걸 얻어와야함
+  const [member, setMember] = useState(null);
   const [params] = useSearchParams();
 
-  return <div>{params.get("id")}님 회원 정보 보기</div>;
+  useEffect(() => {
+    axios
+      .get("/api/member?" + params.toString())
+      .then((response) => setMember(response.data));
+  }, []);
+
+  if (member === null) {
+    return <Spinner />;
+  }
+  return (
+    <Box>
+      <h1>{member.id}님 정보</h1>
+      <FormControl>
+        <FormLabel>password</FormLabel>
+        <Input type="text" value={member.password} readOnly />
+      </FormControl>
+      <FormControl>
+        <FormLabel>email</FormLabel>
+        <Input value={member.email} readOnly />
+      </FormControl>
+      <Button colorScheme="pink">수정</Button>
+      <Button colorScheme="blue">삭제</Button>
+    </Box>
+  );
 }
