fcf85ea65412f13fb7e4c59828d7d15f3c081fa4
yel106 <yel_@hanmail.net>
2023-11-13 10:29:11 +0900

    회원 탈퇴 기능

--- src/page/member/MemberView.js
+++ src/page/member/MemberView.js
@@ -1,5 +1,5 @@
-import { useSearchParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import {
   Box,
   FormControl,
@@ -7,6 +7,15 @@ import {
   Input,
   Spinner,
   Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  useToast,
 } from "@chakra-ui/react";
 import axios from "axios";
 
@@ -14,6 +23,11 @@ export function MemberView() {
   // /member?id=userid 로 넘어오는데 이걸 얻어와야함
   const [member, setMember] = useState(null);
   const [params] = useSearchParams();
+
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
+  const navigate = useNavigate();
+  const toast = useToast();
 
   useEffect(() => {
     axios
@@ -24,6 +38,43 @@ export function MemberView() {
   if (member === null) {
     return <Spinner />;
   }
+
+  function handleDelete() {
+    // axios
+    // delete /api/member?id=userid
+
+    //ok -> home 이동, toast 띄우기
+    // error -> toast 띄우기
+    // finally -> modal 닫기
+
+    axios
+      .delete("/api/member?" + params.toString())
+      .then(() => {
+        toast({
+          description: "회원 탈퇴하였습니다.",
+          status: "success",
+        });
+        navigate("/");
+
+        // TODO: 로그 아웃 기능 추가하기
+        // FIXME : 이 기능 고치기
+      })
+      .catch((error) => {
+        if (error.response.status === 401 || error.response.status === 403) {
+          toast({
+            description: "권한이 없습니다.",
+            status: "error",
+          });
+        } else {
+          toast({
+            description: "탈퇴 처리중에 문제가 발생했습니다.",
+            status: "error",
+          });
+        }
+      })
+      .finally(() => onClose());
+  }
+
   return (
     <Box>
       <h1>{member.id}님 정보</h1>
@@ -36,7 +87,26 @@ export function MemberView() {
         <Input value={member.email} readOnly />
       </FormControl>
       <Button colorScheme="pink">수정</Button>
-      <Button colorScheme="blue">삭제</Button>
+      <Button colorScheme="blue" onClick={onOpen}>
+        탈퇴
+      </Button>
+
+      {/* 삭제 모달*/}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>삭제 확인</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>탈퇴 하시겠습니까?</ModalBody>
+
+          <ModalFooter>
+            <Button onClick={onClose}>닫기</Button>
+            <Button onClick={handleDelete} colorScheme="red">
+              탈퇴
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
