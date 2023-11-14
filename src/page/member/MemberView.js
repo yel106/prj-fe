62c7426322dff9234181fdc88d95b962facb3a64
yel106 <yel_@hanmail.net>
2023-11-14 11:57:07 +0900

    다른 사람 정보 확인시 로그인화면으로 이동, 로그아웃상태에서 정보확인시 로그인화면으로 이동

--- src/page/member/MemberView.js
+++ src/page/member/MemberView.js
@@ -32,7 +32,14 @@ export function MemberView() {
   useEffect(() => {
     axios
       .get("/api/member?" + params.toString())
-      .then((response) => setMember(response.data));
+      .then((response) => setMember(response.data))
+      .catch((error) => {
+        navigate("/login");
+        toast({
+          description: "권한이 없습니다.",
+          status: "warning",
+        });
+      });
   }, []);
 
   if (member === null) {
