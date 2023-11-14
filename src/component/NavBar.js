5525a221492818fafb32b30bb12e2e0818f74483
yel106 <yel_@hanmail.net>
2023-11-14 15:22:24 +0900

    로그인여부에 따라 navbar 버튼 출력 결정

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -26,12 +26,19 @@ export function NavBar() {
   return (
     <Flex>
       <Button onClick={() => navigate("/")}>home</Button>
-      <Button onClick={() => navigate("/write")}>write</Button>
-      <Button onClick={() => navigate("/signup")}>sign up</Button>
-      <Button onClick={() => navigate("/member/list")}>회원목록</Button>
-      <Button onClick={() => navigate("/login")}>로그인</Button>
-      <Button onClick={handleLogout}>로그 아웃</Button>{" "}
-      {/*로그 아웃은 이동할 필요없음*/}
+      {isAuthenticated() && (
+        <Button onClick={() => navigate("/write")}>write</Button>
+      )}
+      {isAuthenticated() || (
+        <Button onClick={() => navigate("/signup")}>signup</Button>
+      )}
+      {isAuthenticated() && (
+        <Button onClick={() => navigate("/member/list")}>회원목록</Button>
+      )}
+      {isAuthenticated() || (
+        <Button onClick={() => navigate("/login")}>로그인</Button>
+      )}
+      {isAuthenticated() && <Button onClick={handleLogout}>로그아웃</Button>}
     </Flex>
   );
 }
