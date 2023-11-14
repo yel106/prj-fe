7df5459522a2c2993b090f566719cd5600a0a81f
yel106 <yel_@hanmail.net>
2023-11-14 14:09:11 +0900

    게시물 작성자의 nickName 표시

--- src/page/board/BoardList.js
+++ src/page/board/BoardList.js
@@ -51,7 +51,7 @@ export function BoardList() {
               >
                 <Td>{board.id}</Td>
                 <Td>{board.title}</Td>
-                <Td>{board.writer}</Td>
+                <Td>{board.nickName}</Td>
                 <Td>{board.inserted}</Td>
               </Tr>
             ))}
--- src/page/board/BoardView.js
+++ src/page/board/BoardView.js
@@ -71,7 +71,7 @@ export function BoardView() {
       </FormControl>
       <FormControl>
         <FormLabel>작성자</FormLabel>
-        <Input value={board.writer} readOnly />
+        <Input value={board.nickName} readOnly />
       </FormControl>
       <FormControl>
         <FormLabel>작성일시</FormLabel>
