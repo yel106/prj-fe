949421f7cfa7d1b958df86b5ce1004b8c042ab9a
yel106 <yel_@hanmail.net>
2023-11-16 16:22:05 +0900

    좋아요 기능

--- src/page/board/BoardView.js
+++ src/page/board/BoardView.js
@@ -4,8 +4,10 @@ import axios from "axios";
 import {
   Box,
   Button,
+  Flex,
   FormControl,
   FormLabel,
+  Heading,
   Input,
   Modal,
   ModalBody,
@@ -21,6 +23,8 @@ import {
 } from "@chakra-ui/react";
 import { LoginContext } from "../../component/LogInProvider";
 import { CommentContainer } from "../../component/CommentContainer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
 
 export function BoardView() {
   const [board, setBoard] = useState(null);
@@ -62,9 +66,22 @@ export function BoardView() {
       .finally(() => onClose());
   }
 
+  function handleLike() {
+    axios
+      .post("/api/like", { boardId: board.id })
+      .then(() => console.log("good"))
+      .catch(() => console.log("bad"))
+      .finally(() => console.log("done"));
+  }
+
   return (
     <Box>
-      <h1>{board.id}번 글 보기</h1>
+      <Flex justifyContent="space-between">
+        <Heading size="xl">{board.id}번 글 보기</Heading>
+        <Button variant="ghost" size="xl" onClick={handleLike}>
+          <FontAwesomeIcon icon={faHeart} size="2xl" />
+        </Button>
+      </Flex>
       <FormControl>
         <FormLabel>제목</FormLabel>
         <Input value={board.title} readOnly />
