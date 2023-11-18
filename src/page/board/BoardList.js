b73a055e570b6fb9a199ca25e94de871806fc8db
yel106 <yel_@hanmail.net>
2023-11-18 16:00:07 +0900

    게시물 목록에 좋아요 수 표시

--- src/page/board/BoardList.js
+++ src/page/board/BoardList.js
@@ -13,6 +13,8 @@ import {
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { ChatIcon } from "@chakra-ui/icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 export function BoardList() {
   const [boardList, setBoardList] = useState(null);
@@ -37,6 +39,9 @@ export function BoardList() {
           <Thead>
             <Tr>
               <Th>id</Th>
+              <Th>
+                <FontAwesomeIcon icon={faHeart} />
+              </Th>
               <Th>title</Th>
               <Th>by</Th>
               <Th>at</Th>
@@ -52,6 +57,7 @@ export function BoardList() {
                 onClick={() => navigate("/board/" + board.id)}
               >
                 <Td>{board.id}</Td>
+                <Td>{board.countLike != 0 && board.countLike}</Td>
                 <Td>
                   {board.title}
                   {board.countComment > 0 && (
