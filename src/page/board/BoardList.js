dd643844b8cc4b534ae448c4ba09927da286cb93
yel106 <yel_@hanmail.net>
2023-11-20 09:12:38 +0900

    11/17

--- src/page/board/BoardList.js
+++ src/page/board/BoardList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import {
   Badge,
   Box,
+  Button,
   Spinner,
   Table,
   Tbody,
@@ -11,21 +12,73 @@ import {
   Tr,
 } from "@chakra-ui/react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, useSearchParams } from "react-router-dom";
 import { ChatIcon } from "@chakra-ui/icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAngleLeft,
+  faAngleRight,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons";
 
-export function BoardList() {
-  const [boardList, setBoardList] = useState(null);
+function Pagination({ pageInfo }) {
+  const pageNumbers = [];
 
   const navigate = useNavigate();
 
+  for (let i = pageInfo.startPageNumber; i <= pageInfo.endPageNumber; i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <Box>
+      {pageInfo.prevPageNumber && (
+        <Button
+          variant="ghost"
+          onClick={() => navigate("/?p=" + pageInfo.prevPageNumber)}
+        >
+          <FontAwesomeIcon icon={faAngleLeft} />
+        </Button>
+      )}
+
+      {pageNumbers.map((pageNumber) => (
+        <Button
+          key={pageNumber}
+          variant={
+            pageNumber === pageInfo.currentPageNumber ? "solid" : "ghost"
+          }
+          onClick={() => navigate("/?p=" + pageNumber)}
+        >
+          {pageNumber}
+        </Button>
+      ))}
+
+      {pageInfo.nextPageNumber && (
+        <Button
+          variant="ghost"
+          onClick={() => navigate("/?p=" + pageInfo.nextPageNumber)}
+        >
+          <FontAwesomeIcon icon={faAngleRight} />
+        </Button>
+      )}
+    </Box>
+  );
+}
+
+export function BoardList() {
+  const [boardList, setBoardList] = useState(null);
+  const [pageInfo, setPageInfo] = useState(null);
+
+  const [params] = useSearchParams();
+  const navigate = useNavigate();
+  const location = useLocation();
+
   useEffect(() => {
-    axios
-      .get("/api/board/list")
-      .then((response) => setBoardList(response.data));
-  }, []);
+    axios.get("/api/board/list?" + params).then((response) => {
+      setBoardList(response.data.boardList);
+      setPageInfo(response.data.pageInfo);
+    });
+  }, [location]);
 
   if (boardList === null) {
     return <Spinner />;
@@ -74,6 +127,8 @@ export function BoardList() {
           </Tbody>
         </Table>
       </Box>
+
+      <Pagination pageInfo={pageInfo} />
     </Box>
   );
 }
